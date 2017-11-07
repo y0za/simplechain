fa467ff1245fda04ad813974730d0a9a735538f5
y0za <yoza.is12s@gmail.com>
2017-11-07 22:13:10 +0900

    Add block.go

---
+++ block.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"time"
+)
+
+// Block is core data structure of blockchain
+type Block struct {
+	Index        int
+	PreviousHash string
+	Timestamp    time.Time
+	Data         []byte
+	Hash         string
+}
+
+// NextBlock create new block from previous block and data
+func NextBlock(prev Block, data []byte) Block {
+	b := Block{
+		Index:        prev.Index + 1,
+		PreviousHash: prev.Hash,
+		Timestamp:    time.Now(),
+		Data:         data,
+	}
+	b.Hash = b.calculateHash()
+	return b
+}
+
+// CheckHash verify whether hash is collect
+func (b Block) CheckHash() bool {
+	return b.caluculateHash() == b.Hash
+}
+
+func (b Block) calculateHash() string {
+	chank := fmt.Sprintf("%d%s%d%s", b.Index, b.PreviousHash, b.Timestamp.Unix(), b.Data)
+	hash := sha256.Sum256([]byte(chank))
+	return string(hash)
+}
