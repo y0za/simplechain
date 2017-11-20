bcc253549c62e2b02d759ddee085c9603f8080e5
y0za <yoza.is12s@gmail.com>
2017-11-20 20:12:09 +0900

    Add GetBlocks method to Env struct

--- http.go
+++ http.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 // Env inject dependencies to http handler
 type Env struct {
 	bc *Blockchain
+}
+
+func (e *Env) GetBlocks(w http.ResponseWriter, r *http.Request) {
+	err := json.NewEncoder(w).Encode(e.bc.chain)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+	}
 }
