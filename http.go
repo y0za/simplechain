00c5edcd8e6fb2ea4eca89ab084bb40daab997d9
y0za <yoza.is12s@gmail.com>
2017-11-24 03:43:31 +0900

    Add AddPeer method to Env struct

--- http.go
+++ http.go
@@ -11,9 +11,26 @@ type Env struct {
 	hub *Hub
 }
 
+// peerJSON used in AddPeer for reading request body
+type peerJSON struct {
+	Peer string `json:"peer"`
+}
+
 func (e *Env) GetBlocks(w http.ResponseWriter, r *http.Request) {
 	err := json.NewEncoder(w).Encode(e.bc.chain)
 	if err != nil {
 		http.Error(w, http.StatusText(500), 500)
 	}
+}
+
+func (e *Env) AddPeer(w http.ResponseWriter, r *http.Request) {
+	var pj peerJSON
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&pj)
+	if err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	connectToPeer(e.hub, e.bc, pj.Peer)
 }
