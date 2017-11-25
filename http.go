8df91ced60f9e4c43d9fddad3146502b8bf718f3
y0za <yoza.is12s@gmail.com>
2017-11-25 17:53:12 +0900

    Add GetPeers method to Env struct

--- http.go
+++ http.go
@@ -23,6 +23,18 @@ func (e *Env) GetBlocks(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (e *Env) GetPeers(w http.ResponseWriter, r *http.Request) {
+	peers := make([]string, 0, len(e.hub.clients))
+	for c, _ := range e.hub.clients {
+		peers = append(peers, c.conn.RemoteAddr().String())
+	}
+
+	err := json.NewEncoder(w).Encode(peers)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+	}
+}
+
 func (e *Env) AddPeer(w http.ResponseWriter, r *http.Request) {
 	var pj peerJSON
 	decoder := json.NewDecoder(r.Body)
