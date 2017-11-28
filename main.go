8c521bb11a126d1bcc9c1653e07263256de3fb51
y0za <yoza.is12s@gmail.com>
2017-11-28 22:56:34 +0900

    Implement main function

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
+	bc := NewBlockchain(GenesisBlock())
+	hub := newHub()
+	go hub.run()
+	env := &Env{bc, hub}
+
+	api := newApiServer(env, ":3001")
+	p2p := newApiServer(env, ":6001")
+
+	go func() {
+		if err := api.ListenAndServe(); err != http.ErrServerClosed {
+			log.Print(err)
+		}
+	}()
+	go func() {
+		if err := p2p.ListenAndServe(); err != http.ErrServerClosed {
+			log.Print(err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	api.Shutdown(ctx)
+	p2p.Shutdown(ctx)
 }
 
-func newApiServer(env Env, addr string) *http.Server {
+func newApiServer(env *Env, addr string) *http.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/blocks", env.GetBlocks)
 	mux.HandleFunc("/mineBlock", env.MineBlock)
@@ -20,7 +52,7 @@ func newApiServer(env Env, addr string) *http.Server {
 	}
 }
 
-func newP2PServer(env Env, addr string) *http.Server {
+func newP2PServer(env *Env, addr string) *http.Server {
 	return &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			serveWs(env.hub, env.bc, w, r)
